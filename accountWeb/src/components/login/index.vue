6b9d3518790abc4e77cc85b22dec3cea82bd479b
zhangxiaoyunSX4539 <zhangxiaoyunSX4539@autohome.com.cn>
2021-05-10 17:40:34 +0800

    test

--- accountWeb/src/components/login/index.vue
+++ accountWeb/src/components/login/index.vue
@@ -9,7 +9,7 @@
                 name="phone"
                 label="手机号"
                 placeholder="请输入手机号"
-                :rules="[{ required: true, message: '手机号不能为空' }]"
+                :rules="[{ required: true, message: '手机号不能为空' },{ pattern: /^1[3456789]\d{9}$/, message: '手机号码格式错误！'}]"
             />
             <van-field
                 v-model="password"
@@ -17,7 +17,7 @@
                 name="password"
                 label="密码"
                 placeholder="请输入"
-                :rules="[{ onSubmit}]"
+                :rules="[{ required: true, message: '密码不能为空'}]"
                 :error-message="errorMsg"
             />
             <div style="margin: 16px;">
@@ -40,7 +40,8 @@
                 name="phone"
                 label="手机号"
                 placeholder="请输入手机号"
-                :rules="[{ required: true, message: '手机号不能为空' }]"
+                :rules="[{ required: true, message: '手机号不能为空' },{ pattern: /^1[3456789]\d{9}$/, message: '手机号码格式错误！'}]"
+                
             />
             <van-field
                 v-model="password"
@@ -48,7 +49,16 @@
                 name="password"
                 label="密码"
                 placeholder="请输入"
-                :rules="[{ required: true, message: '密码不能为空' }]"
+                :rules="[{ required: true, message: '密码不能为空' },{ pattern: /^[0-9]{6,16}$/, message: '密码在6~16位之间'}]"
+            />
+            <van-field
+                v-model="pwd"
+                type="password"
+                name="pwd"
+                label="确认密码"
+                placeholder="请输入"
+                :rules="[{ required: true, message: '确认密码不能为空' }]"
+                :error-message="errMsgs"
             />
             <div style="margin: 16px;">
                 <van-button round block type="info" native-type="submit">注册</van-button>
@@ -75,8 +85,11 @@ export default {
             login:true,
             tel:'',
             password:'',
+            pwd:'',
             name:'',
-            errorMsg:''
+            errorMsg:'',
+            errMsg:'',
+            errMsgs:''
         }
     },
     methods:{
@@ -95,19 +108,25 @@ export default {
                     localStorage.setItem('user',JSON.stringify(user))
                     this.$root.goLogin=false
                 }else{
-                    this.errorMsg='密码或账号不正确'
+                    this.errorMsg='密码或手机号错误'
                 }
             })
             
         },
         onSubmit1(values) {
-            registerFun(values).then(res=>{
+            if(values.password==values.pwd){
+                registerFun(values).then(res=>{
                 let data = res.data;
                 if(data&&data.ok){
                     this.login=true
                     return data.msg;
+                }else{
+                    this.errMsg='该手机号已注册'
                 }
             })
+            }else{
+                this.errMsgs='确认密码错误'
+            }
         }
     }
 }
